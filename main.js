77962b9a65d5aaf3c22e2e1d0efa1aec829b3695
Thalion1 <gustav.kodehode@gmail.com>
2026-03-26 11:09:18 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+function goToWebsite() {
+    alert("this website is not created by me");
+    location.replace("https://robertsspaceindustries.com/en/enlist?jumpto=https%3A%2F%2Frobertsspaceindustries.com%2Fen%2Fstar-citizen");
+}
