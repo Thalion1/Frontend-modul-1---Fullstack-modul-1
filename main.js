5a4ca08947e20b40bc260370dc8a9be800bc98da
Thalion1 <gustav.kodehode@gmail.com>
2026-03-26 12:03:01 +0100

    popup fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function goToWebsite() {
-    alert("this website is not created by me");
+    alert("the website you are goung to\nis not created by me");
     location.replace("https://robertsspaceindustries.com/en/enlist?jumpto=https%3A%2F%2Frobertsspaceindustries.com%2Fen%2Fstar-citizen");
 }
